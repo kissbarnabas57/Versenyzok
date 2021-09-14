1df5fd36ec9bbae474163618c1876409dad13338
kissbarnabas57 <90251406+kissbarnabas57@users.noreply.github.com>
2021-09-14 09:37:20 +0200

    Add project files.

---
+++ Versenyzők/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Versenyzők
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MasodikFeladat();
+            HarmadikFeladat();
+            NegyedikFeladat();
+            ÖtödikFeladat();
+            HatodikFeladat();
+            HetedikFeladat();
+
+            Console.ReadKey();
+        }
+
+        private static void HetedikFeladat()
+        {
+            
+        }
+
+        private static void HatodikFeladat()
+        {
+            
+        }
+
+        private static void ÖtödikFeladat()
+        {
+            
+        }
+
+        private static void NegyedikFeladat()
+        {
+            
+        }
+
+        private static void HarmadikFeladat()
+        {
+            
+        }
+
+        private static void MasodikFeladat()
+        {
+            
+        }
+    }
+}
