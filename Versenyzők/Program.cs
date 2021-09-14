8fef9cd2b3c8b9dc74a083fdc763707a5d076301
kissbarnabas57 <90251406+kissbarnabas57@users.noreply.github.com>
2021-09-14 11:40:52 +0200

    Részleges megoldás

--- Versenyzők/Program.cs
+++ Versenyzők/Program.cs
@@ -9,6 +9,7 @@ namespace Versenyzők
 {
     class Program
     {
+        static List<lista> pilotak = new List<lista>();
         static void Main(string[] args)
         {
             MasodikFeladat();
@@ -23,32 +24,39 @@ namespace Versenyzők
 
         private static void HetedikFeladat()
         {
-            
+            Console.WriteLine("7. feladat: ");
         }
 
         private static void HatodikFeladat()
         {
-            
+            Console.WriteLine("6. feladat: ");
         }
 
         private static void ÖtödikFeladat()
         {
-            
+            Console.WriteLine("5. feladat: ");
         }
 
         private static void NegyedikFeladat()
         {
-            
+            Console.WriteLine("4. feladat: {0}", pilotak.Last().Nev);
         }
 
         private static void HarmadikFeladat()
         {
-            
+            Console.WriteLine("3. feladat: {0}", pilotak.Count());
         }
 
         private static void MasodikFeladat()
         {
-            
+            Console.WriteLine("2. feladat: ");
+            StreamReader be = new StreamReader("pilotak.csv");
+            be.ReadLine();
+            while (!be.EndOfStream)
+            {
+                pilotak.Add(new lista(be.ReadLine()));
+            }
+            be.Close();
         }
     }
 }
---
+++ Versenyzők/lista.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Versenyzők
+{
+    class lista
+    {
+        private int nev;
+
+        public int Nev
+        {
+            get { return nev; }
+            set { nev = value; }
+        }
+        private int szuletesi_datum;
+
+        public int Szuletesi_datum
+        {
+            get { return szuletesi_datum; }
+            set { szuletesi_datum = value; }
+        }
+        private int nemzetiseg;
+
+        public int Nemzetiseg
+        {
+            get { return nemzetiseg; }
+            set { nemzetiseg = value; }
+        }
+        private int rajtszam;
+
+        public int Rajtszam
+        {
+            get { return rajtszam; }
+            set { rajtszam = value; }
+        }
+
+        public lista(int nev, int szuletesi_datum, int nemzetiseg, int rajtszam)
+        {
+            this.nev = nev;
+            this.szuletesi_datum = szuletesi_datum;
+            this.nemzetiseg = nemzetiseg;
+            this.rajtszam = rajtszam;
+        }
+    }
+}
